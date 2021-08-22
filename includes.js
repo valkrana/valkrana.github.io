883c87223dbd20173e0fb9554b708f0755f78e7d
ActualSailorCat <valkrana.miraquinal@gmail.com>
2021-08-22 03:47:45 -0400

    Return to Top

--- includes.js
+++ includes.js
@@ -1,4 +1,4 @@
-var navs =	'<a href="index.html">Home</a>' +
+var navs =	'<a href="index.html" id="Top">Home</a>' +
 			'<a href="gallery.html">Gallery</a>' +
 			'<a href="about.html">About</a>' +
 			'<a href="voice.html">Voice</a>' +
@@ -10,8 +10,9 @@ function navigation() {
     document.getElementById('navs').innerHTML = navs
 }
 
-var foot =	'<img src="css/skull.png"><img src="css/skull.png" style="padding: 0 5px 0 5px"><img src="css/skull.png"><br>' +
-			'<p>Character by <a href="http://www.twitter.com/ActualSailorCat" target="_blank">SailorCat</a>';
+var foot =	'<a href="#Top"><p>Return to Top</a><br><br>' +
+			'<img src="css/skull.png"><img src="css/skull.png" style="padding: 0 5px 0 5px"><img src="css/skull.png"><br>' +
+			'<p style="margin-top: -10px">Character by <a href="http://www.twitter.com/ActualSailorCat" target="_blank">SailorCat</a>';
 
 function footer() {  
     document.getElementById('footer').innerHTML = foot
